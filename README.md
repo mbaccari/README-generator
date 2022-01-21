e99fffce062e2d5aa3635183fd5f0c4a85de145e
Marshall Baccari <mbaccari@uw.edu>
2022-01-21 00:50:47 -0800

    finishing project. got functions for getting the license links and badges and updated the template to correctly format once uploaded. Added links to reach sections from TOC

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# README-generator
+## README file generator
+
+![License: Mozilla](https://img.shields.io/badge/License-Mozilla-yellow.svg)
+
+--[Table of Contents]--
+
+1.[Description](#description)
+
+2.[Install](#install)
+
+3.[Instructions](#instructions)
+
+4.[Credits](#credits)
+
+5.[Questions](#email)
+
+## Description
+
+This is to create a professional and effective readme from a template to more efficiently and concisely create a readme file for a project 
+
+
+## Install
+
+To install, just download the js file off github and open in vs code
+
+
+## Instructions
+
+To use, open the folder in a bash terminal and run "node index.js" and fill out the prompts to create your readme file
+
+
+## Credits
+
+Marshall Baccari (me)
+
+
+## License
+
+This project is protected under Mozilla Public License 2.0.
+
+![License: mpl-2.0](https://choosealicense.com/licenses/mpl-2.0)
+
+
+## Questions
+
+-If you have questions, feel free to email me at marshall.d.baccari@gmail.com
+
+-Check out my github profile at https://github.com/mbaccari
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,43 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-
+// getting correct format to put in badge link
 const getLicense = (license) => {
+  console.log(license)
   if (license === 'GNU AGPLv3') {
-    return 'GNU_AGPLv3'
+    return 'agpl-3.0'
   } else if (license === 'GNU GPLv3') {
-    return 'GNU_GPLv3'
+    return 'gpl-3.0'
   } else if (license === 'GNU LGPLv3') {
-    return 'GNU_LGPLv3'
+    return 'lgpl-3.0'
   } else if (license === 'Mozilla Public License 2.0') {
     return 'Mozilla'
   } else if (license === 'Apache License 2.0') {
     return 'Apache'
   } else if (license === 'MIT License') {
     return 'MIT'
+  } else {
+    return "";
   }
 };
+// getting correct format to put into a link
+const licenseLink = (license) => {
+  if (license === 'GNU AGPLv3') {
+    return 'GNU-AGPLv3'
+  } else if (license === 'GNU GPLv3') {
+    return 'GNU-GPLv3'
+  } else if (license === 'GNU LGPLv3') {
+    return 'GNU-LGPLv3'
+  } else if (license === 'Mozilla Public License 2.0') {
+    return 'mpl-2.0'
+  } else if (license === 'Apache License 2.0') {
+    return 'apache-2.0'
+  } else if (license === 'MIT License') {
+    return 'mit'
+  } else {
+    return "";
+  }
+}
+
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
 
 function renderLicenseBadge(license) {
     return `![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)`
@@ -26,16 +48,18 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+    return `![License: ${license}](https://choosealicense.com/licenses/${license})`
+}
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (data) =>{
 
+const link = renderLicenseLink(licenseLink(data.license))
+
 const badge = renderLicenseBadge(getLicense(data.license));
+
+
 
 return `##${data.title}  
 
@@ -43,27 +67,48 @@ ${badge}
 
 --[Table of Contents]--
 
-    1.[Description](#description)
-    2.[Install](#install)
-    3.[Instructions](#instructions)
-    4.[Credits](#credits)
-    5.[Questions](#email)
+1.[Description](#description)
 
-##Description
+2.[Install](#install)
+
+3.[Instructions](#instructions)
+
+4.[Credits](#credits)
+
+5.[Questions](#email)
+
+## Description
+
 ${data.description}
 
-##Install
+
+## Install
+
 ${data.install}
 
-##Instructions
+
+## Instructions
+
 ${data.instructions}
 
-##Credits
+
+## Credits
+
 ${data.credits}
 
-For any questions, contact me here:
--${data.email}
--${data.github}`
+
+## License
+
+This project is protected under ${data.license}.
+
+${link}
+
+
+## Questions
+
+-If you have questions, feel free to email me at ${data.email}
+
+-Check out my github profile at https://github.com/${data.github}`
 };
 
 module.exports = generateMarkdown;
